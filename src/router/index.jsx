e55e7ee6e5c8863a849fd303d8e8155b910bdb7c
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-08-29 23:08:13 +0300

    Add authentication page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import HomePage from './../pages/homePage'
+import Authentication from "./../pages/authentication"
 
 export default () => {
     return (
         <Switch>
             <Route path='/' component={HomePage} exact />
-            <Route path='/login' component={HomePage} />
-            <Route path='/register' component={HomePage} />
+            <Route path='/login' component={Authentication} />
+            <Route path='/register' component={Authentication} />
         </Switch>
     )
 }
