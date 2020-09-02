155166870b424169403235925e967063a5adea56
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-09-02 21:12:18 +0300

    Add Home page some styles

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import styles from './homePage.module.scss'
+import {Link, NavLink} from 'react-router-dom'
+import logo from './logo-color.jpg'
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <>
+            <header>
+                <Link to='/' className={styles.brand}>
+                    <img src={logo} alt="Logo"/>
+                </Link>
+                <ul className={styles.links}>
+                    <li className={styles.linkItem}>
+                        <NavLink to='/login' className={styles.link} activeStyle={{
+                            fontWeight: "bold",
+                            color: "black"
+                        }}>
+                            Login
+                        </NavLink>
+                    </li>
+                    <li className={styles.linkItem}>
+                        <NavLink to='/register' className={styles.link} activeStyle={{
+                            fontWeight: "bold",
+                            color: "black"
+                        }}>
+                            Signup
+                        </NavLink>
+                    </li>
+                </ul>
+            </header>
+        </>
     )
 }
 
